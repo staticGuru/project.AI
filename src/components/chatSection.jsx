e799164da2b13aa2f64c48db43c93268a3e19934
Guruvignesh M <vigneshguru274@gmail.com>
2023-08-27 00:22:00 +0530

    add the chat sections value

--- src/components/chatSection.jsx
+++ src/components/chatSection.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { FaHandPaper, FaHome, FaLightbulb, FaClock } from "react-icons/fa";
+import { BsBoxes } from "react-icons/bs";
+import { MdOutlineDescription, MdCall } from "react-icons/md";
+import { BiMessageDots } from "react-icons/bi";
 export function ChatSection() {
   return (
-    <div class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 mb-50">
       <div class="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
         <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
           <div class="flex w-full mt-2 space-x-3 max-w-xs">
@@ -105,12 +108,39 @@ export function ChatSection() {
           </div>
         </div>
 
-        <div class="bg-gray-300 p-4">
+        <div className="bg-gray-300 p-4 flex justify-center gap-2">
           <input
-            class="flex items-center h-10 w-full rounded px-3 text-sm"
+            className="flex items-center h-10 w-full rounded-full outline-none px-3 text-sm"
             type="text"
-            placeholder="Type your message…"
+            placeholder="Ask a question..."
           />
+          <button className="inline-flex items-center mt-1 justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-black rounded-full focus:shadow-outline hover:bg-gray-300">
+            <FaHandPaper />
+          </button>
+        </div>
+        <div class="bg-gray-200 p-1 flex gap-2 flex-row items-center justify-center">
+          <button className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-white rounded-md focus:shadow-outline hover:bg-gray-300">
+            <FaHome color="black" />
+          </button>
+          <button className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-white rounded-md focus:shadow-outline hover:bg-gray-300">
+            <BsBoxes color="black" />
+          </button>
+          <button className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-white rounded-md focus:shadow-outline hover:bg-gray-300">
+            <FaLightbulb color="black" />
+          </button>
+          <button className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-white rounded-md focus:shadow-outline hover:bg-gray-300">
+            <BiMessageDots color="black" />
+          </button>
+          <button className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-white rounded-md focus:shadow-outline hover:bg-gray-300">
+            <MdOutlineDescription color="black" />
+          </button>
+
+          <button className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-white rounded-md focus:shadow-outline hover:bg-gray-300">
+            <MdCall color="black" />
+          </button>
+          <button className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-white rounded-md focus:shadow-outline hover:bg-gray-300">
+            <FaClock color="black" />
+          </button>
         </div>
       </div>
     </div>
