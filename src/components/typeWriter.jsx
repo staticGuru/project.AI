4b44be2fe103fbc1fb1b2035db2ab7a8941a6dfe
Guruvignesh M <vigneshguru274@gmail.com>
2023-09-05 19:20:10 +0530

    provide the onEvent press

--- src/components/typeWriter.jsx
+++ src/components/typeWriter.jsx
@@ -1,7 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const Typewriter = ({ text, delay, infinite }) => {
-  const [currentText, setCurrentText] = useState('');
+export const Typewriter = ({
+  text,
+  delay,
+  isUser=false,
+  infinite,
+  onKeyDown = () => {},
+}) => {
+  const [currentText, setCurrentText] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -9,18 +15,27 @@ export const Typewriter = ({ text, delay, infinite }) => {
 
     if (currentIndex < text.length) {
       timeout = setTimeout(() => {
-        setCurrentText(prevText => prevText + text[currentIndex]);
-        setCurrentIndex(prevIndex => prevIndex + 1);
+        setCurrentText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
       }, delay);
-
-    } else if (infinite) { // ADD THIS CHECK
+    } else if (infinite) {
+      // ADD THIS CHECK
       setCurrentIndex(0);
-      setCurrentText('');
+      setCurrentText("");
     }
 
     return () => clearTimeout(timeout);
   }, [currentIndex, delay, infinite, text]);
-
+  if (isUser) {
+    return (
+      <input
+        className="flex items-center h-10 w-full bg-transparent outline-none px-3 text-sm"
+        type="text"
+        placeholder=""
+        value={currentText}
+        onKeyDown={onKeyDown}
+      />
+    );
+  }
   return <span>{currentText}</span>;
 };
-
